4c0485f6e2436ed96fb0edeacc408f75cc06b574
Jeffrey Douangpaseuth <jdouangpaseuth@hotmail.ca>
2020-01-08 16:43:56 -0500

    update samples

--- streamlistener-basic/src/test/java/demo/ModuleApplicationTests.java
+++ streamlistener-basic/src/test/java/demo/ModuleApplicationTests.java
@@ -18,6 +18,7 @@ package demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
